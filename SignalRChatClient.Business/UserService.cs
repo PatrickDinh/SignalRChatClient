bc46f05c9114b2622de29d4e1f567582b5d31495
Patrick Dinh <d.conghoang@gmail.com>
2015-04-26 18:55:27 +1000

    add business logic layer

---
+++ SignalRChatClient.Business/UserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalRChatClient.Data;
+using SignalRChatClient.Domain;
+
+namespace SignalRChatClient.Business
+{
+    public class UserService
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void CreateUser(User user)
+        {
+            _userRepository.Insert(user);
+        }
+    }
+}
